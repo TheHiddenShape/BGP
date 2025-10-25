edce654a9757fe12ff8d6fb47e8ff51965555241
aschiffe <ammon.pharaoh@outlook.fr>
2025-10-26 01:51:51 +0200

    Document BGP EVPN VXLAN fabric setup

--- P3/router/leaf_router3.sh
+++ P3/router/leaf_router3.sh
@@ -5,6 +5,19 @@ ip link set dev vxlan10 up
 brctl addif br0 vxlan10
 brctl addif br0 eth0
 
+# no ipv6 forwarding -> Disable IPv6 routing (IPv4 only configuration)
+
+# router bgp 1 -> Configure BGP with AS number 1
+
+## neighbor 1.1.1.1 remote-as 1 -> Establish a BGP session with the router whose IP address is 1.1.1.1, which belongs to Autonomous System (AS) number 1
+## neighbor 1.1.1.1 update-source lo -> Use the IP address of the loopback interface (lo) as the source address to establish the BGP TCP connection to this neighbor
+
+## address-family l2vpn evpn -> Enter EVPN address family configuration mode for Layer 2 VPN over BGP
+### neighbor 1.1.1.1 activate -> Enable EVPN address family for this BGP neighbor
+### advertise-all-vni -> Automatically advertise all locally configured VNIs via EVPN (Type 3 IMET routes)
+
+# router ospf -> Enable OSPF process for underlay IP connectivity
+
 vtysh <<-EOF
 configure terminal
   no ipv6 forwarding
--- P3/router/spine_router.sh
+++ P3/router/spine_router.sh
@@ -1,3 +1,22 @@
+# no ipv6 forwarding -> Disable IPv6 routing (IPv4 only configuration)
+
+# interfaces ethX -> link to leaf N
+# interfaces lo -> loopback BGP / OSPF
+
+# router bgp 1 -> Configure BGP with AS number 1
+
+## neighbor ibgp peer-group -> Create a peer group named ibgp for easier management of multiple BGP neighbors
+## neighbor ibgp remote-as 1 -> All peers in this group belong to AS 1 (iBGP)
+## neighbor ibgp update-source lo -> Use loopback as source IP for all BGP sessions in this peer group
+## bgp listen range 1.1.1.0/29 peer-group ibgp -> Dynamically accept BGP connections from any IP in range 1.1.1.0-1.1.1.7 and assign them to ibgp peer group
+
+## address-family l2vpn evpn -> Enter EVPN address family configuration mode
+### neighbor ibgp activate -> Enable EVPN address family for all peers in the ibgp peer group
+### neighbor ibgp route-reflector-client -> Configure this Spine as a Route Reflector for all ibgp peers (reflects EVPN routes between Leafs)
+
+# router ospf -> Enable OSPF process for underlay routing
+## network 0.0.0.0/0 area 0 -> Advertise all interfaces into OSPF area 0 (ensures loopback and all physical links are included)
+
 vtysh <<-EOF
 configure terminal
   no ipv6 forwarding
