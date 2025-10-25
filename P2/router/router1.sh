59ececb3f26cc2126c7deb1c79f6a5a1f51de5d6
Ammon <60857611+TheHiddenShape@users.noreply.github.com>
2025-10-26 01:15:37 +0200

    Document STATIC and DYNAMIC modes in router1.sh
    Added comments explaining STATIC and DYNAMIC modes for VXLAN configuration.

--- P2/router/router1.sh
+++ P2/router/router1.sh
@@ -7,6 +7,12 @@ ip link set dev br0 up
 ip addr add 10.1.1.1/24 dev eth0
 # ip addr show eth0
 
+# Mode STATIC = Manual end 2 end tunnel
+# characteristics: You know the IP address of the other VTEP in advance, 1-to-1 tunnel only, Manual configuration of each peer
+
+# Mode DYNAMIC = Automatic Multi-Point Discovery
+# characteristics: You don't know the other VTEPs in advance, Automatic 1-to-many tunnel Automatic discovery via multicast (FLOOD AND LEARN)
+
 if [ "$1" = "static" ]; then
     ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789
 elif [ "$1" = "dynamic" ]; then
