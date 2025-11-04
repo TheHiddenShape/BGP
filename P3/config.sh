d6ba3f57ca0f7e4067c3b66526bcb84999a00396
Ammon <60857611+TheHiddenShape@users.noreply.github.com>
2025-11-04 10:52:01 +0100

    feat: Refactor container configuration to avoid arbitrary exec order

--- P3/config.sh
+++ P3/config.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
 
+# SPINE CONFIG
 for container_id in $(docker ps -q); do
   node_type=$(docker inspect --format='{{ index .Config.Labels "node_type" }}' "$container_id")
+  hostname=$(docker inspect --format='{{ .Config.Hostname }}' "$container_id")
 
-  if [[ "$node_type" == "router" || "$node_type" == "host" ]]; then
-    hostname=$(docker inspect --format='{{ .Config.Hostname }}' "$container_id")
-    echo "Checking container $container_id with hostname '$hostname' and node_type '$node_type'"
-    if [[ "$node_type" == "router" && "$hostname" =~ -1$ ]]; then
-      echo "Container $container_id ($hostname): executing router-1 (leaf router) vxlan config"
+  if [[ "$node_type" == "router" && "$hostname" =~ -4$ ]]; then
+    echo "Container $container_id ($hostname): executing router-4 (spine router) bgp evpn vxlan config"
+    cat ./router/spine_router4.sh | docker exec -i $container_id bash -s
+  fi
+done
+
+# LEAFS CONFIG
+for container_id in $(docker ps -q); do
+  node_type=$(docker inspect --format='{{ index .Config.Labels "node_type" }}' "$container_id")
+  hostname=$(docker inspect --format='{{ .Config.Hostname }}' "$container_id")
+
+  if [[ "$node_type" == "router" ]]; then
+    if [[ "$hostname" =~ -1$ ]]; then
+      echo "Container $container_id ($hostname): executing router-1 (leaf router) bgp evpn vxlan config"
       cat ./router/leaf_router1.sh | docker exec -i $container_id bash -s
-    elif [[ "$node_type" == "router" && "$hostname" =~ -2$ ]]; then
-      echo "Container $container_id ($hostname): executing router-2 (leaf router) vxlan config"
+    elif [[ "$hostname" =~ -2$ ]]; then
+      echo "Container $container_id ($hostname): executing router-2 (leaf router) bgp evpn vxlan config"
       cat ./router/leaf_router2.sh | docker exec -i $container_id bash -s
-    elif [[ "$node_type" == "router" && "$hostname" =~ -3$ ]]; then
-      echo "Container $container_id ($hostname): executing router-3 (leaf router) vxlan config"
+    elif [[ "$hostname" =~ -3$ ]]; then
+      echo "Container $container_id ($hostname): executing router-3 (leaf router) bgp evpn vxlan config"
       cat ./router/leaf_router3.sh | docker exec -i $container_id bash -s
-    elif [[ "$node_type" == "router" && "$hostname" =~ -4$ ]]; then
-      echo "Container $container_id ($hostname): executing router-4 (spine router) vxlan config"
-      cat ./router/spine_router4.sh | docker exec -i $container_id bash -s
-    elif [[ "$node_type" == "host" && "$hostname" =~ -1$ ]]; then
+    fi
+  fi
+done
+
+# HOST CONFIG
+for container_id in $(docker ps -q); do
+  node_type=$(docker inspect --format='{{ index .Config.Labels "node_type" }}' "$container_id")
+  hostname=$(docker inspect --format='{{ .Config.Hostname }}' "$container_id")
+
+  if [[ "$node_type" == "host" ]]; then
+    if [[ "$hostname" =~ -1$ ]]; then
       echo "Container $container_id ($hostname): executing host-1 ip addr config"
       cat ./host/host1.sh | docker exec -i $container_id bash -s
-    elif [[ "$node_type" == "host" && "$hostname" =~ -2$ ]]; then
+    elif [[ "$hostname" =~ -2$ ]]; then
       echo "Container $container_id ($hostname): executing host-2 ip addr config"
       cat ./host/host2.sh | docker exec -i $container_id bash -s
-    elif [[ "$node_type" == "host" && "$hostname" =~ -3$ ]]; then
+    elif [[ "$hostname" =~ -3$ ]]; then
       echo "Container $container_id ($hostname): executing host-3 ip addr config"
       cat ./host/host3.sh | docker exec -i $container_id bash -s
-    else
-      echo "Container $container_id ($hostname): hostname does not match -1 / -2 / -3 / -4 pattern"
     fi
   fi
 done
